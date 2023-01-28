ca053a846debf8169650e26617dc148b7f157491
angelicacvo <angiemarin0707@gmail.com>
2023-01-28 15:08:04 -0500

    carousel config

---
+++ src/app/body/fav-movies.ts
@@ -0,0 +1,6 @@
+export interface favMovies{
+    score: number;
+    name: string;
+    watchList: string;
+    trailer: string;
+}
--- src/app/body/favs/favs.component.ts
+++ src/app/body/favs/favs.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./favs.component.css']
 })
 export class FavsComponent {
-  img: string = "./assets/img/1917.png"
+  img1: string = "./assets/img/1917.png"
+  img2: string = "./assets/img/avatar.png"
+  img3: string = "./assets/img/black-swan.png"
+  img4: string = "./assets/img/dora.png"
+  img5: string = "./assets/img/endgame.png"
+  img6: string = "./assets/img/ironman.png"
+  img7: string = "./assets/img/jaws.png"
+  img8: string = "./assets/img/joker.png"
+  img9: string = "./assets/img/ladybird.png"
+  img10: string = "./assets/img/moonlight.png"
+  img11: string = "./assets/img/outih.png"
+  img12: string = "./assets/img/shangchi.png"
 }
