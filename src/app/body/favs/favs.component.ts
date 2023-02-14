90f8b9da59fdc5b553c09d7d9cdea14cb5e080ef
fedehc18 <fedehc18@hotmail.com>
2023-02-13 22:07:26 -0500

    Responsive del boton carrousel

--- src/app/body/favs/favs.component.ts
+++ src/app/body/favs/favs.component.ts
@@ -1,15 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { FavMovie } from '../fav-movies';
 
-
 @Component({
   selector: 'app-favs',
   templateUrl: './favs.component.html',
-  styleUrls: ['./favs.component.css']
+  styleUrls: ['./favs.component.css'],
 })
 export class FavsComponent {
-
   @Input() favMovies: FavMovie[] = [];
-
-
 }
